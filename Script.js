4f1b280a982b923e1a31ba9a9e65d1e64e9b7760
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-04-12 20:00:29 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
 
+var cards = $(".grid");
+
+for(var i = 0; i < cards.length; i++){
+
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+
+    cards.eq(target).before(cards.eq(target2));
+
+}
+
