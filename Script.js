a6979d4f821511c307e0bda598bafea73bb5b381
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-04-12 17:45:44 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,30 @@
-$(".myItems").html($(".myItems .item").sort(Function() {
-                                            return Math.random() - 0.5;
-                   }));
+    $(document).ready(function(){
+
+          $('div#sponsors').each(function(){
+
+                // get current div
+                var $div_parent = $(this);
+
+                // get array of childs in parent div
+                var $divsArr = $div_parent.children('div.single-sponsor');
+
+                // sort array of childs in parent div (#sponsors) randomly
+                $divsArr.sort(function(a,b){
+
+                      // Get a random number between 0 and 10
+                      var temp = parseInt( Math.random()*10 );
+
+                      // Get 1 or 0, whether temp is odd or even
+                      var isOddOrEven = temp%2;
+
+                      // Get +1 or -1, whether temp greater or smaller than 5
+                      var isPosOrNeg = temp>5 ? 1 : -1;
+
+                      // Return -1, 0, or +1
+                      return( isOddOrEven*isPosOrNeg );
+                })
+
+                // append child items to parent
+                .appendTo($div_parent);            
+          });
+    });
