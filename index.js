e24e3fb433967cea6233bd1874edac9509d00478
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-04-14 10:22:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ $cont.append(shuffle(itemsArr))
 function shuffle(array) {
     for (let i = (array).length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [(array)[i], (array)[j]] = [(array)[j], (array)[i]];
     }
-  return array;
+  return (array);
 }
 
 </script>
