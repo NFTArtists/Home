82c67f499ce3399db54796f3ed3445a6b11ac2f3
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-05-12 11:09:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.body.addEventListener("touchstart", playVideo);
-    function playVideo() {
-        const video = document.getElementById('myVideo');
-        if(video.playing) {
-        } else {
-            video.play();
-        }
-    }
-});
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
