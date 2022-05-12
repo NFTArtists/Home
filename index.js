4ce6743581a58fdbb12d628575bfe9d8fd75c99a
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-05-12 09:56:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ function shuffle(array) {
   return (array);
 }
 }
+
+$(document).ready(function() {
+  $('#headerVideoLink').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });         
+});
