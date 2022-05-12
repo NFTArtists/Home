44ea4d1fa92c2dd65be1e6c550993bad7c3ddf96
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-05-12 10:58:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-do
+document.addEventListener("DOMContentLoaded", function(){
+    document.body.addEventListener("touchstart", playVideo);
+    function playVideo() {
+        const video = document.getElementById('myVideo');
+        if(video.playing) {
+        } else {
+            video.play();
+        }
+    }
+});
