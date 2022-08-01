5884ffb935a42886056e36038b4efccfab8e3012
NFTArtists <102387609+NFTArtists@users.noreply.github.com>
2022-08-01 11:47:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function shuffle(array) {
   return (array);
 }
 }
+
+
+function myFunction() {
+  var x = document.getElementById("Hide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
